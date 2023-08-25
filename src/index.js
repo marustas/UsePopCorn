198663ef7992fe2ca00709e1f6fb466fd1ed2fa7
Stanislau Marudau <marudov.stas@gmail.com>
2023-08-26 00:44:03 +0200

    Added a star component

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating></StarRating>
+    <StarRating maxRating={5} />
     {/*<App /> */}
   </React.StrictMode>
 );
