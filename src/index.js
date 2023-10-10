27861f958685e8de3fa2c6cfadea9c62a5d5c446
Stanislau Marudau <marudov.stas@gmail.com>
2023-10-10 19:41:41 +0200

    Fixed the bug with the React.propTypes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <StarRating
-      maxRating="stas"
+      maxRating = {5}
       messageRating={["Terrible", "Bad", "Okay", "Good", "Awesome"]}
     />
     {/*<App /> */}
